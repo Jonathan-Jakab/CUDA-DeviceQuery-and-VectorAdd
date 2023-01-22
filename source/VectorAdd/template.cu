1a4e82f88868c2e5d7785ef736fcdfa13bc34a1b
Jonathan J <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2023-01-21 21:26:29 -0800

    Add files via upload

---
+++ source/VectorAdd/template.cu
@@ -0,0 +1,97 @@
+#include <cuda_runtime.h> 
+#include <device_launch_parameters.h> 
+#include <wb.h>
+
+__global__ void vecAdd(float *in1, float *in2, float *out, int len) {
+  //@@ Insert code to implement vector addition here
+	int i = threadIdx.x + blockDim.x * blockIdx.x;
+	if (i < len) {
+
+		out[i] = in1[i] + in2[i];
+	}
+}
+
+int main(int argc, char **argv) {
+  wbArg_t args;
+  int size;
+  int inputLength;
+  float *hostInput1;
+  float *hostInput2;
+  float *hostOutput;
+  float *deviceInput1;
+  float *deviceInput2;
+  float *deviceOutput;
+
+  args = wbArg_read(argc, argv);
+
+  wbTime_start(Generic, "Importing data and creating memory on host");
+  hostInput1 = (float *)wbImport(wbArg_getInputFile(args, 0), &inputLength);
+  hostInput2 = (float *)wbImport(wbArg_getInputFile(args, 1), &inputLength);
+  hostOutput = (float *)malloc(inputLength * sizeof(float));
+  
+  size = inputLength * sizeof(float);	//We need to allocate memory that is the appropriate size. Using Float is also architecture dependant so using sizeof(float) will make sure that enough space is allocated as per whatever platform its running on.
+  
+  wbTime_stop(Generic, "Importing data and creating memory on host");
+
+  wbLog(TRACE, "The input length is ", inputLength);
+
+  wbTime_start(GPU, "Allocating GPU memory.");
+  //@@ Allocate GPU memory here
+  cudaMalloc((void**) &deviceInput1, size);
+  cudaMalloc((void**) &deviceInput2, size);
+  cudaMalloc((void**) &deviceOutput, size);
+
+  wbTime_stop(GPU, "Allocating GPU memory.");
+
+  wbTime_start(GPU, "Copying input memory to the GPU.");
+  //@@ Copy memory to the GPU here
+  cudaMemcpy(deviceInput1, hostInput1, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceInput2, hostInput2, size, cudaMemcpyHostToDevice);
+
+  wbTime_stop(GPU, "Copying input memory to the GPU.");
+
+  //@@ Initialize the grid and block dimensions here
+	dim3 DimGrid((inputLength - 1) / 256 + 1, 1, 1);
+	dim3 DimBlock(256, 1, 1);
+
+  wbTime_start(Compute, "Performing CUDA computation");
+  //@@ Launch the GPU Kernel here
+  vecAdd<<<DimGrid, DimBlock>>>(deviceInput1 , deviceInput2 , deviceOutput ,  inputLength);
+  
+  cudaDeviceSynchronize();
+  wbTime_stop(Compute, "Performing CUDA computation");
+
+  wbTime_start(Copy, "Copying output memory to the CPU");
+  //@@ Copy the GPU memory back to the CPU here
+  cudaMemcpy(hostOutput, deviceOutput, size, cudaMemcpyHostToDevice);
+
+  wbTime_stop(Copy, "Copying output memory to the CPU");
+
+  wbTime_start(GPU, "Freeing GPU Memory");
+  //@@ Free the GPU memory here
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
+
+  wbTime_stop(GPU, "Freeing GPU Memory");
+
+  wbSolution(args, hostOutput, inputLength);
+
+  free(hostInput1);
+  free(hostInput2);
+  free(hostOutput);
+
+  return 0;
+}
+
+
+
+
+/*
+Use to test Data Set 0
+-e .\VectorAdd\Dataset\0\output.raw -i .\VectorAdd\Dataset\0\input0.raw,.\VectorAdd\Dataset\0\input1.raw -o .\VectorAdd\Dataset\0\myoutput.raw -t vector > .\VectorAdd\Dataset\0\result.txt
+
+
+Use to test Data Set 1
+-e .\VectorAdd\Dataset\1\output.raw -i .\VectorAdd\Dataset\1\input0.raw,.\VectorAdd\Dataset\1\input1.raw -o .\VectorAdd\Dataset\1\myoutput.raw -t vector > .\VectorAdd\Dataset\1\result.txt
+*/
