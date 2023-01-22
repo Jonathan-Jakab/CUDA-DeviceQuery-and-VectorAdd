895860ea90b0c7d3b1954b43c9bcdda96746aefc
Jonathan J <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2023-01-21 21:35:58 -0800

    Delete build directory

--- build/DeviceQuery/sources.cmake
+++
@@ -1,3 +0,0 @@
-
-add_lab("DeviceQuery")
-add_lab_template("DeviceQuery" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
--- build/VectorAdd/sources.cmake
+++
@@ -1,2 +0,0 @@
-add_lab("VectorAdd")
-add_lab_template("VectorAdd" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
